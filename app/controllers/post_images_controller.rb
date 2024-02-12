36671773ce0f85c8c3ddb58d5eb28139fd9ba622
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-12 04:46:18 +0000

    [Finish] 9章投稿機能を作成しよう

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,29 @@
+class PostImagesController < ApplicationController
+  def top
+  end
+
+  def new
+    @post_image = PostImage.new
+  end
+
+　def create
+　  @post_image = PostImage.new(post_image_params)
+　  @post_image.user_id = current_user.id
+　  @post_image.save
+　  redirect_to post_images_path
+　end
+
+  def show
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+end
