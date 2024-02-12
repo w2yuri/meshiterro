b914e53ffa31a1eedfe1257f4dfca63cb59edb70
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-12 09:28:26 +0000

    [Finish]　１３章

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,19 +6,26 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
-　def create
-　  @post_image = PostImage.new(post_image_params)
-　  @post_image.user_id = current_user.id
-　  @post_image.save
-　  redirect_to post_images_path
-　end
+  def create
+  @post_image = PostImage.new(post_image_params)
+  @post_image.user_id = current_user.id
+  @post_image.save
+  redirect_to post_images_path
+  end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def index
     @post_images = PostImage.all
   end
+  
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to @post_images_path
+  end 
 
   private
 
